595059568cc910e5d58ece001740928fb246b499
Sagar Srivastav <sagsriva@cisco.com>
2017-05-27 05:21:22 -0700

    server client code
    Signed-off-by: Sagar Srivastav <sagsriva@cisco.com>

---
+++ chat-server/constants.h
@@ -0,0 +1,11 @@
+/*
+ * constants.h
+ * This file will store the constants used in the program
+ */
+
+#define SERVER_PORT 5000
+
+#define MAX_SERV_SEND_BUF_SIZE 1025
+#define MAX_SERV_RECV_BUF_SIZE 1025
+#define MAX_CLNT_SEND_BUF_SIZE 1025
+#define MAX_CLNT_RECV_BUF_SIZE 1025
